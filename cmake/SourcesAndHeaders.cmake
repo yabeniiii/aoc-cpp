de274f57dee37cad144c5b13f59f79e49184148a
Aidan Boland <aidanboland1@icloud.com>
2023-10-01 14:28:56 +0200

    customised

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,16 +1,15 @@
 set(sources
-    src/tmp.cpp
+  src/main.cpp
 )
 
 set(exe_sources
-		src/main.cpp
-		${sources}
+  src/main.cpp
+  ${sources}
 )
 
 set(headers
-    include/project/tmp.hpp
+  include/template/main.hpp
 )
 
 set(test_sources
-  src/tmp_test.cpp
 )
