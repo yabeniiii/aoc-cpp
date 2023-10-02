8d21d01b0581a93e87e24d3325410aeda51b699a
Aidan Boland <aidanboland1@icloud.com>
2023-10-03 00:57:13 +0200

    day 1 part 2

---
+++ src/day1part2.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char **argv) {
+  if (argc <= 1 || argc > 2) {
+    std::cerr << "Invalid number of arguments" << std::endl;
+    return 1;
+  }
+
+  std::ifstream inputFile(argv[1]);
+
+  if (!inputFile.is_open()) {
+    std::cerr << "Error opening file" << std::endl;
+    return 1;
+  }
+
+  std::vector<int> totals;
+  std::string line;
+  int current = 0;
+  while (std::getline(inputFile, line)) {
+    if (line.empty()) {
+      totals.push_back(current);
+      current = 0;
+    } else {
+      int number = std::stoi(line);
+      current += number;
+    }
+  }
+  totals.push_back(current);
+  current = 0;
+
+  inputFile.close();
+
+  std::sort(totals.begin(), totals.end());
+
+  std::cout << (totals.at(totals.size() - 1) + totals.at(totals.size() - 2) + totals.at(totals.size() - 3)) << std::endl;
+}
